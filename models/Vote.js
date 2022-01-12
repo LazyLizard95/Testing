89192ea97000a41a165ce91ea5dced10a4845c06
LazyLizard95 <51461957+LazyLizard95@users.noreply.github.com>
2022-01-12 13:54:46 -0500

    testing models and shit

---
+++ models/Vote.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Vote extends Model {}
+
+Vote.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'post',
+        key: 'id'
+      }
+    },
+    comment_id: {
+      type: DataTypes.INTEGER,
+      references: {
+      model: 'comment',
+      key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'vote'
+  }
+);
+
+module.exports = Vote;
